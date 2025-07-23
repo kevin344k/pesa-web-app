6e5dbaac5fc9ef23de463f13bf46be6194b20ff7
kevin344k <kevin344k@gmail.com>
2025-07-23 14:16:22 -0500

    se arega porcentajes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Modal from "./components/ModalChangeStatus";
 import { Toaster } from "react-hot-toast";
+import NumberFlow from "@number-flow/react";
+import svg_sort from "./assets/sort.svg";
 dayjs.extend(relativeTime);
 
 let socket;
@@ -14,6 +16,7 @@ export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [lineSelect, setLineSelect] = useState({});
   const [loading, setLoading] = useState(true);
+  const [sort, setSort] = useState("asc");
   const [, forceUpdate] = useState(0); // 👈 esto fuerza el re-render
 
   useEffect(() => {
@@ -42,6 +45,7 @@ export default function App() {
   useEffect(() => {
     socket.on("initial_data", (machines) => {
       console.log("Datos iniciales recibidos:", machines);
+
       // Aquí actualizas tu estado o UI
       setMachines(machines);
       setLoading(false); // ✅ Oculta el loader cuando llega la data
@@ -67,6 +71,24 @@ export default function App() {
     setLineSelect(data);
   };
 
+  const handleSort = () => {
+    setSort((prev) => (prev === "asc" ? "desc" : "asc"));
+
+    //Aqui ordenamos
+    const sorted = [...machines].sort((a, b) => {
+      const aPercent = a.meta === 0 ? 0 : (a.fabricado / a.meta) * 100;
+      const bPercent = b.meta === 0 ? 0 : (b.fabricado / b.meta) * 100;
+
+      if (sort === "asc") {
+        return aPercent - bPercent;
+      } else {
+        return bPercent - aPercent;
+      }
+    });
+    console.log(sort);
+    setMachines(sorted);
+  };
+
   // 👇 Este es el useEffect que fuerza re-render cada minuto
   useEffect(() => {
     const interval = setInterval(() => {
@@ -79,7 +101,16 @@ export default function App() {
     <>
       <Toaster position="top-right" />
       <div className="bg-primary-night min-h-screen p-6">
-        <p className="text-white text-center my-2 mb-6">REAL TIME APP</p>
+        <p className="text-white text-center my-1 ">REAL TIME APP</p>
+        <div className=" flex justify-end mb-2">
+          <div
+            onClick={handleSort}
+            className="p-1 hover:cursor-pointer active:scale-95 
+           border border-neutral-200 rounded-md "
+          >
+            <img className="w-3 " src={svg_sort} alt="" />
+          </div>
+        </div>
         {loading ? (
           <div className="flex flex-col justify-start items-center min-h-screen bg-primary-night">
             <div className="w-16 h-16 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
@@ -96,14 +127,14 @@ export default function App() {
                 <div
                   className={`absolute inset-0  w-full h-full rounded-lg shadow-md  active:scale-95 transition-transform duration-100 ${
                     machine.status === "RUN"
-                      ? "text-gray-300 bg-green-600 animate-pulse"
+                      ? "text-gray-300 bg-green-700 animate-pulse"
                       : machine.status === "STOP"
                       ? "text-gray-300 bg-red-600 animate-pulse"
                       : "text-gray-300 bg-neutral-600 "
                   }`}
                 ></div>
-                <div className="relative z-10 w-full h-full  flex justify-between items-center p-2 ">
-                  <div className="flex flex-col gap-3">
+                <div className="relative z-10 w-full h-full  flex justify-between items-center p-1 ">
+                  <div className="flex flex-col gap-2">
                     <div className="flex flex-col gap-1">
                       <p className="font-bold text-white text-left text-center">
                         {machine.name}
@@ -117,25 +148,29 @@ export default function App() {
                         dayjs(machine.updated_at).fromNow()}
                     </p>
                   </div>
-               {
-                machine.meta===null||machine.fabricado===null?(""):(
-                     <div className="flex gap-3 flex-col justify-between  items-center ">
-                  <div className="flex flex-col text-center ">
-                      <span className="text-3xl text-neutral-200">
-                      {((machine.fabricado / machine.meta) * 100).toFixed()}%
-                    </span>
-                    <span className="text-xs text-neutral-200">
-                      {machine.fabricado} fdn
-                    </span>
-                  </div>
-                    <div>
-                      <p className="text-xs text-neutral-900">
+                  {machine.meta === null || machine.fabricado === null ? (
+                    ""
+                  ) : (
+                    <div className="flex gap-3 flex-col justify-between  items-center ">
+                      <div className="flex flex-col text-center ">
+                        <span className="text-2xl text-neutral-200">
+                          <NumberFlow
+                            value={(
+                              (machine.fabricado / machine.meta) *
+                              100
+                            ).toFixed()}
+                          />
+                          <span className="text-md">%</span>
+                        </span>
+                        <p className="text-xs text-neutral-200">
+                          {machine.fabricado} fdn
+                        </p> <span className="text-xs text-neutral-900">
                         meta: {machine.meta} fdn
-                      </p>
+                        </span>
+                      </div>
+                   
                     </div>
-                  </div>
-                )
-               }
+                  )}
                 </div>
               </div>
             ))}
