581b40dc97d494b5379dbd226e41701894ddc7e5
kevin344k <kevin344k@gmail.com>
2025-07-25 16:24:51 -0500

    ee

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -22,46 +22,43 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="p-4 bg-primary-night ">
+    <div className="p-4 bg-primary-night min-h-screen">
       <h1 className="text-xl text-white text-center font-bold mb-4">Paralizaciones</h1>
+
       {loading ? (
-        <p>Cargando...</p>
+        <div className="flex justify-center items-center h-40">
+          <div className="w-8 h-8 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+        </div>
       ) : logs.length === 0 ? (
-        <p>No se encontraron registros.</p>
+        <p className="text-center text-white">No se encontraron registros.</p>
       ) : (
         <ul className="space-y-2">
-         {logs
-  .slice() // para no mutar el estado original
-  .sort((a, b) => b.duracion_horas - a.duracion_horas) // orden descendente
-  .map((log, i) => {
-    const descripcion =
-      paralizaciones.find(
-        (p) => p.codigo === Number(log.paralizacion)
-      )?.descripcion || "Descripción no encontrada";
+          {logs
+            .slice()
+            .sort((a, b) => b.duracion_horas - a.duracion_horas)
+            .map((log, i) => {
+              const descripcion =
+                paralizaciones.find(
+                  (p) => p.codigo === Number(log.paralizacion)
+                )?.descripcion || "Descripción no encontrada";
 
-
-      console.log(logs);
-      
-    return (
-      <li
-        key={i}
-        className="p-3 flex bg-secondary-night justify-between text-white rounded shadow-sm border-l-4 border-red-400"
-      >
-        <div>
-          <p>
-            {log.machine_name}
-          </p>
-          <p className="text-sm">
-            <strong>Motivo:</strong> {descripcion}
-          </p>
-        </div>
-        <div className="bg-red-400 flex items-center min-w-[50px] p-1 text-md">
-          {log.duracion_horas} <span className="text-xs"> hrs.</span>
-        </div>
-      </li>
-    );
-  })}
-
+              return (
+                <li
+                  key={i}
+                  className="p-3 flex bg-secondary-night justify-between text-white rounded shadow-sm border-l-4 border-red-400"
+                >
+                  <div>
+                    <p>{log.machine_name}</p>
+                    <p className="text-sm">
+                      <strong>Motivo:</strong> {descripcion}
+                    </p>
+                  </div>
+                  <div className="bg-red-400 flex items-center min-w-[50px] p-1 text-md">
+                    {log.duracion_horas} <span className="text-xs"> hrs.</span>
+                  </div>
+                </li>
+              );
+            })}
         </ul>
       )}
     </div>
